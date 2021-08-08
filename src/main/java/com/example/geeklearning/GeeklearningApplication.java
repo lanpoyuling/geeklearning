d96d0d0d0641f42e984d79c0292621180b56846c
10932 <1093221911@qq.com>
2021-08-08 20:14:40 +0800

    init

---
+++ src/main/java/com/example/geeklearning/GeeklearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.geeklearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeklearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeeklearningApplication.class, args);
+    }
+
+}
